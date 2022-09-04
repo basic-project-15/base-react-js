612b94bc717591f45b701e04dae19bb36eccf07b
Luis Fernando Solano Martínez <luisfer.sm15@gmail.com>
2022-09-03 19:43:15 -0600

    :sparkles: Agregando estado global
    Creando estado global de inicio de sesion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+// Hooks
+import { AuthContext, authReducer } from './hooks';
 
 // Components
 import { Typography } from '@mui/material';
 
 const App = () => {
+  const [auth, dispatchAuth] = useReducer(authReducer, {});
+
   return (
-    <div className="flex w-full bg-blue-400 h-screen justify-center items-center">
-      <Typography
-        variant="h1"
-        component="h2"
-        style={{ fontFamily: 'Poppins-Regular' }}
-      >
-        Hola Mundo
-      </Typography>
-    </div>
+    <AuthContext.Provider value={{ auth, dispatchAuth }}>
+      <div className="flex w-full bg-blue-400 h-screen justify-center items-center">
+        <Typography
+          variant="h1"
+          component="h2"
+          style={{ fontFamily: 'Poppins-Regular' }}
+        >
+          Hola Mundo
+        </Typography>
+      </div>
+    </AuthContext.Provider>
   );
 };
 
