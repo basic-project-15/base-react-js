3cc11784610364fcc03799edd3b7fffa169d5f70
Luis Fernando Solano Martínez <luisfer.sm15@gmail.com>
2022-09-03 18:40:03 -0600

    :lipstick: Agregando fuentes
    Agregando fuentes de Poppins

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import { Typography } from '@mui/material';
 const App = () => {
   return (
     <div className="flex w-full bg-blue-400 h-screen justify-center items-center">
-      <Typography variant="h1" component="h2">
+      <Typography
+        variant="h1"
+        component="h2"
+        style={{ fontFamily: 'Poppins-Regular' }}
+      >
         Hola Mundo
       </Typography>
     </div>
