31fcf415f02bd7ee0441f1ed57622bb4d7853b85
XxLuisFer15xX <luisfer.sm15@gmail.com>
2022-09-22 16:08:06 -0600

    :lipstick: Creando componente DatePickerCustom

--- src/components/atoms/index.jsx
+++ src/components/atoms/index.jsx
@@ -1,6 +1,7 @@
 export { default as ButtonCustom } from './ButtonCustom';
 export { default as CheckBoxCustom } from './CheckBoxCustom';
 export { default as ControlLabelCustom } from './ControlLabelCustom';
+export { default as DatePickerCustom } from './DatePickerCustom';
 export { default as DrawerItem } from './DrawerItem';
 export { default as FormControlCustom } from './FormControlCustom';
 export { default as IconButtonCustom } from './IconButtonCustom';
--- src/components/pages/ComponentsInputs4/index.jsx
+++ src/components/pages/ComponentsInputs4/index.jsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 
 // Components
 import { Divider } from '@mui/material';
-import { SelectCustom, TextCustom } from '../../atoms';
+import { DatePickerCustom, SelectCustom, TextCustom } from '../../atoms';
 
 // Const
 import { constGeneros } from '../../../common/constants';
 
 const ComponentsInputs4 = () => {
   const [genero, setGenero] = useState('');
+  const [fechaNacimiento, setFechaNacimiento] = useState(null);
 
   return (
     <div className="pb-4 flex flex-col">
@@ -24,6 +25,22 @@ const ComponentsInputs4 = () => {
             options={constGeneros}
             value={genero}
             setValue={setGenero}
+            required
+          />
+        </div>
+        <Divider />
+      </div>
+      <TextCustom text="DatePickers" className="text-6xl mt-4" />
+      <Divider />
+      {/* Estados */}
+      <div className="px-4 pt-4">
+        <TextCustom text="Estados" className="text-3xl" />
+        <Divider />
+        <div className="flex flex-col gap-1">
+          <DatePickerCustom
+            name="Fecha de Nacimeinto"
+            value={fechaNacimiento}
+            setValue={setFechaNacimiento}
             required
           />
         </div>
