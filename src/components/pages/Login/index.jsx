8a18042aa25e3c84ef3a37e69c1ec437061fcbd3
XxLuisFer15xX <luisfer.sm15@gmail.com>
2022-09-26 08:05:36 -0600

    :lipstick: Diseño de Login
    Diseñando la interfaz de usuario para la pantalla de Login.

--- src/components/pages/Login/index.jsx
+++ src/components/pages/Login/index.jsx
@@ -1,27 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 // Hooks
 import { AuthContext } from '../../../hooks/context';
 
 // Components
-import { Button } from '@mui/material';
+import { ButtonCustom, TextCustom, TextInputCustom } from '../../atoms';
 
 // Const
 import { typesActionsAuth } from '../../../common/types';
 
 const Login = () => {
   const { dispatchAuth } = useContext(AuthContext);
+  const [correo, setCorreo] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogin = () => {
     dispatchAuth({ type: typesActionsAuth.authLogin });
   };
 
   return (
-    <div>
-      <h1 className="mb-4">Login</h1>
-      <Button variant="outlined" onClick={handleLogin}>
-        Iniciar sesión
-      </Button>
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-700">
+      <div className="bg-white w-96 px-6 py-8 flex flex-col gap-4 rounded-xl">
+        <TextCustom
+          text="Inicio de sesión"
+          className="self-center text-2xl fontPBold color-general rounded-lg"
+        />
+        <TextInputCustom
+          name="Correo"
+          type="email"
+          value={correo}
+          setValue={setCorreo}
+        />
+        <TextInputCustom
+          name="Contraseña"
+          type="password"
+          value={password}
+          setValue={setPassword}
+        />
+        <ButtonCustom
+          text="Ingresar"
+          onClick={handleLogin}
+          className="w-full"
+          typeColor="primary"
+        />
+      </div>
     </div>
   );
 };
